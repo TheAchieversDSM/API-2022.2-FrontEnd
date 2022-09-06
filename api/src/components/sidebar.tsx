fda8ce2fc3933883fa19863cacbd8639dd4421c1
Priscila  Silva <prialmeida1801@gmail.com>
2022-09-06 14:14:14 -0300

    ajustes sidebar

--- api/src/components/sidebar.tsx
+++ api/src/components/sidebar.tsx
@@ -1,7 +1,5 @@
-//import useState hook to create menu collapse state
 import React, { useState } from "react";
 
-//import react pro sidebar components
 import {
   ProSidebar,
   Menu,
@@ -12,7 +10,7 @@ import {
 } from "react-pro-sidebar";
 
 //import icons from react icons
-import { FaList, FaRegHeart } from "react-icons/fa";
+import { FaCartPlus, FaList, FaRegHeart } from "react-icons/fa";
 import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
 import { RiPencilLine } from "react-icons/ri";
 import { BiCog } from "react-icons/bi";
@@ -25,10 +23,8 @@ import "./sidebar.css";
 
 const Header = () => {
   
-    //create initial menuCollapse state using useState hook
     const [menuCollapse, setMenuCollapse] = useState(false)
 
-    //create a custom function that will change menucollapse state from false to true and true to false
   const menuIconClick = () => {
     //condition checking to change state from true to false and vice versa
     menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
@@ -42,7 +38,7 @@ const Header = () => {
           <SidebarHeader>
           <div className="logotext">
               {/* small and big change using menucollapse state */}
-              <p>{menuCollapse ? "Logo" : "Big Logo"}</p>
+              <p>{menuCollapse ? "Olá" : "Olá Usuário(a)"}</p>
             </div>
             <div className="closemenu" onClick={menuIconClick}>
                 {/* changing menu collapse icon on click */}
@@ -56,17 +52,20 @@ const Header = () => {
           <SidebarContent>
             <Menu iconShape="square">
               <MenuItem active={true} icon={<FiHome />}>
-                Home
+                Inicio
               </MenuItem>
-              <MenuItem icon={<FaList />}>Category</MenuItem>
-              <MenuItem icon={<FaRegHeart />}>Favourite</MenuItem>
-              <MenuItem icon={<RiPencilLine />}>Author</MenuItem>
-              <MenuItem icon={<BiCog />}>Settings</MenuItem>
+              <MenuItem icon={<FaList />}>Categorias</MenuItem>
+              <MenuItem icon={<FaRegHeart />}>Favoritos</MenuItem>
+              <MenuItem icon={<FaCartPlus />}>Produtos</MenuItem>
+              <MenuItem icon={<RiPencilLine />}>Cadastro</MenuItem>
+              <MenuItem icon={<BiCog />}>Configurações</MenuItem>
+              <MenuItem icon={<FiLogOut />}>Sair</MenuItem>
             </Menu>
           </SidebarContent>
           <SidebarFooter>
             <Menu iconShape="square">
-              <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+              <p>Termos de Uso</p>
+              <p>Politica de Privacidade</p>
             </Menu>
           </SidebarFooter>
         </ProSidebar>
