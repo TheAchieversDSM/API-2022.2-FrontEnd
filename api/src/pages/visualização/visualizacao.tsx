db30b530952a071e11d2b704babbdb999d30afdb
Mariana Tamay <maritamay4@gmail.com>
2022-09-06 18:58:35 -0300

    Inserção do carrosel e finalização da estilização

--- api/src/pages/visualização/visualizacao.tsx
+++ api/src/pages/visualização/visualizacao.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Botao from '../../components/button';
 import Navigation from '../../components/navbar';
 import Outline from '../../components/outlinebutton';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 import './visu.css'
 
 
 
 export default function Visualizacao() {
+
+    
+
     return (
         <>
             <Navigation/>
@@ -15,7 +20,7 @@ export default function Visualizacao() {
                 <div className="principal">
                     <h1 className="name">NOME DO PRODUTO</h1>
                     <div className="descricao">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
                     </div>
                     <h2 className="preço">R$ 230,00</h2>
                     <Botao/>
@@ -38,13 +43,113 @@ export default function Visualizacao() {
                             <div className="col-3">
                                 <div className="pacotes"></div>
                                 <h3>R$ 180,00</h3>
-                                <Outline/>
+                                <Outline />
                             </div>
                         </div>
                     </div>
                 </div>
                 <div>
                     <h2 className="sugestao">Outras sugestões</h2>
+                    <AliceCarousel>
+                        <div className="yours-custom-class container">
+                            <div className="row">
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="yours-custom-class container">
+                            <div className="row">
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="yours-custom-class container">
+                            <div className="row">
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="yours-custom-class container">
+                            <div className="row">
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                                <div className="card col-4">
+                                    <div className="card-img"></div>
+                                    <div className="card-botao">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </AliceCarousel>
                 </div>
             </div>
         </>
