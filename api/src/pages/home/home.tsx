c606099e3d9bbb0c3ffb9472ce5702fcbf1b4649
Mariana Tamay <maritamay4@gmail.com>
2022-09-14 14:02:12 -0300

    Integração na página de visualização

--- api/src/pages/home/home.tsx
+++ api/src/pages/home/home.tsx
@@ -6,12 +6,22 @@ import Navigation from '../../components/navbar';
 
 import './home.css'
 
+
+let modelo = [
+    {
+        'id':'',
+        'nome':'',
+        'preco':'',
+        'descricao':''
+    }
+]
+
 export default function Home() {
-    const [produtos,setProdutos] = useState([Object])
+    const [produtos,setProdutos] = useState(modelo)
 
     useEffect(()=>{
         async function render() { 
-        axios.get(`http://localhost:8080/produtos/pegarTodosProdutos`, ).then((res)=>{
+        axios.get(`http://localhost:8080/produtos/pegarTodosProdutos`).then((res)=>{
             setProdutos(res.data)
             console.log(res.data)
             })
@@ -45,12 +55,11 @@ export default function Home() {
                      <div className="card-img"></div>
 
                      <div className="nome-prod">
-                        <h4>Nome do Produto</h4>
-                        {/*<h4>{produto.nome}</h4>*/}
+                        <h4>{produto.nome}</h4>
                     </div>
                       
                      <div className="card-botao">
-                         <Button type="submit"><Link to="/produto/id">Ver Produto!</Link></Button>
+                         <Button type="submit"><Link to={`/produto/${produto.id}`} >Ver Produto!</Link></Button>
                      </div>
 
                  </div>
--- api/src/routes/routes.tsx
+++ api/src/routes/routes.tsx
@@ -15,7 +15,7 @@ const Routes = () => {
                     <Route path="/criacao-produto" element={<Produto />} />
                     <Route path="/" element={<Home/>}/>
                     <Route path="/login" element={<Login/>}/>   
-                    <Route path="/produto" element={<Visualizacao/>}/>
+                    <Route path="/produto/:id" element={<Visualizacao/>}/>
                     <Route path="/criacao-promocao" element={<Promocao />} /> 
                     <Route path="/criacao-produto" element={<Produto />} />
                 </Switch>
