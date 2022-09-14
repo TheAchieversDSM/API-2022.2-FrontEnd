68c68dacdf131b5fb9d517b34f4d44d37062722e
aluno <aluno>
2022-09-14 12:09:02 -0300

    inclusao da tela de serviços e ajustes da sidebar

--- api/src/routes/routes.tsx
+++ api/src/routes/routes.tsx
@@ -4,9 +4,9 @@ import { Route, BrowserRouter, Routes as Switch } from "react-router-dom";
 import Produto from "../pages/criacao-produto/produto";
 import Visualizacao from "../pages/visualizacao/visualizacao";
 import Promocao from "../pages/criacao-promocao/promocao";
+import Servico from "../pages/criacao-servico/servico";
 import Login from "../pages/login/login";
 import Home from "../pages/home/home";
-import Pacote from "../pages/criacao-pacote/pacote";
 
 const Routes = () => {
     return (
@@ -19,7 +19,7 @@ const Routes = () => {
                     <Route path="/produto" element={<Visualizacao/>}/>
                     <Route path="/criacao-promocao" element={<Promocao />} /> 
                     <Route path="/criacao-produto" element={<Produto />} />
-                    <Route path="/criacao-pacote" element={<Pacote />} />
+                    <Route path="/criacao-servico" element={<Servico />} />
                 </Switch>
             </Fragment>
         </BrowserRouter>
