990410627bde27f59f889442faf3e0d9c3c44fa8
Mariana Tamay <maritamay4@gmail.com>
2022-09-25 12:16:07 -0300

    Adição da tabela e área de sugestões na página carrinho

--- api/src/pages/carrinho/carrinho.tsx
+++ api/src/pages/carrinho/carrinho.tsx
@@ -1,9 +1,72 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Navigation from "../../components/navbar";
+import './carrinho.css'
+
 
 export default function Carrinho() {
     return(
         <>
         <Navigation/>
+        <div className="cont1">
+            <div className="row">
+                <div className="listac col-9">
+                  <table className="table table-hover">
+                  <tbody>
+                    <tr>
+                      <th scope="row">
+                        <div className="iconeimg"></div>
+                      </th>
+                      <td>Nome do prod/serv</td>
+                      <td className="preco1">Preço</td>
+                    </tr>
+                    <tr>
+                      <th scope="row"><div className="iconeimg"></div></th>
+                      <td>Nome do prod/serv</td>
+                      <td className="preco1">Preço</td>
+                    </tr>
+                    <tr>
+                      <th scope="row"><div className="iconeimg"></div></th>
+                      <td >Nome do prod/serv</td>
+                      <td className="preco1">Preço</td>
+                    </tr>
+                  </tbody>
+                  </table>
+                  <h2 className="precototal">Preço total</h2>
+                </div>
+                <div className="sug col-3">
+                  <div className="maissug">
+                    <div className="card sugest">
+                      <div className="card-imgc"></div>
+                      <div className="nome-prod">
+                        <h5>Nome do produto</h5>
+                      </div>
+                      <div className="card-botao">
+                        <Button type="submit"><Link to={""} >Ver Produto!</Link></Button>
+                      </div>
+                    </div>
+                    <div className="card sugest">
+                      <div className="card-imgc"></div>
+                      <div className="nome-prod">
+                        <h5>Nome do produto</h5>
+                      </div>
+                      <div className="card-botao">
+                        <Button type="submit"><Link to={""} >Ver Produto!</Link></Button>
+                      </div>
+                    </div>
+                    <div className="card sugest">
+                      <div className="card-imgc"></div>
+                      <div className="nome-prod">
+                        <h5>Nome do produto</h5>
+                      </div>
+                      <div className="card-botao">
+                        <Button type="submit"><Link to={""} >Ver Produto!</Link></Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
