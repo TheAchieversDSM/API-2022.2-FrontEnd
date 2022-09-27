2f52b4fc38a443fc34b363144b4ef06c58c6cc38
Priscila  Silva <prialmeida1801@gmail.com>
2022-09-27 12:09:52 -0300

    integração back e fron tas listagens pacote e promoção

--- api/src/pages/listagens/listagens.tsx
+++ api/src/pages/listagens/listagens.tsx
@@ -6,6 +6,14 @@ import Sidebar from "../../components/sidebar";
 
 import "./listagens.css"
 
+let modeloPromo = [
+    {
+        'id': '',
+        'nome': '',
+        'descricao': ''
+    }
+]
+
 let modelo = [
     {
         'id': '',
@@ -17,7 +25,7 @@ let modelo = [
 
 export default function Listagens() {
     const [produtos, setProdutos] = useState(modelo)
-    const [servicos, setServicos] = useState(modelo)
+    const [promocoes, setPromocoes] = useState(modeloPromo)
 
     useEffect(() => {
         let url = window.location.href.split("/")
@@ -28,6 +36,16 @@ export default function Listagens() {
         }
         else if (url[3] === "servicos") {
             axios.get(`http://localhost:8080/servicos/pegarTodosServicos`).then((res) => {
+                setProdutos(res.data)
+            })
+        }
+        else if (url[3] === "pacotes") {
+            axios.get(`http://localhost:8080/pacotes/pegarTodosPacotes`).then((res) => {
+                setProdutos(res.data)
+            })
+        }
+        else if (url[3] === "promocoes") {
+            axios.get(`http://localhost:8080/promocoes/pegarTodasPromocoes`).then((res) => {
                 setProdutos(res.data)
             })
         }
@@ -51,6 +69,16 @@ export default function Listagens() {
                         <Button variant="outline-primary" href="servicos" disabled>Serviços</Button>
                         : 
                         <Button variant="outline-primary" href="servicos">Serviços</Button>
+                    }  
+                    {window.location.href.split("/")[3] == "pacotes" ? 
+                        <Button variant="outline-primary" href="pacotes" disabled>Pacotes</Button>
+                        : 
+                        <Button variant="outline-primary" href="pacotes">Pacotes</Button>
+                    }  
+                    {window.location.href.split("/")[3] == "promocoes" ? 
+                        <Button variant="outline-primary" href="promocoes" disabled>Promoções</Button>
+                        : 
+                        <Button variant="outline-primary" href="promocoes">Promoções</Button>
                     }                      
                 </div>
 
@@ -58,14 +86,23 @@ export default function Listagens() {
                     <thead>
                         <tr>
                             <th>Nome</th>
+                            {window.location.href.split('/')[3] == 'pacotes' ?
+                            <th>Descrição</th>
+                            :
                             <th>Preço</th>
+                            }
                         </tr>
                     </thead>
                     <tbody>
                         {produtos.map(produto =>
                             <tr>
                                 <td>{produto.nome}</td>
+                                {window.location.href.split('/')[3] == 'pacotes' ?
+                                <td>{produto.descricao}</td>
+                                :
                                 <td>R$ {produto.preco}</td>
+                            }
+                                
                             </tr>
                         )}
                     </tbody>
