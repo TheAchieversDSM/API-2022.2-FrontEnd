2282483068904f2b4fe1a7952507c6cc18deb780
Mariana Tamay <maritamay4@gmail.com>
2022-10-03 16:46:51 -0300

    Ajustes no componente alerta

--- api/src/components/alerta.tsx
+++ api/src/components/alerta.tsx
@@ -1,17 +1,24 @@
-import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 'react';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import Alert from 'react-bootstrap/Alert';
 
-function AlertaProm(props: { prom: string }) {
-  return (
-    <>
-      {['primary'].map((variant) => (
-        <Alert key={variant} variant={variant} className="alerta">
-            <Alert.Heading>Confira essa promoçao!</Alert.Heading>
-            <p>{props.prom}</p>
-        </Alert>
-      ))}
-    </>
-  );
+function AlertaProm(props: any) {
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert variant="primary" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Confira esta promoção!</Alert.Heading>
+        <p>
+          {props.prom}
+        </p>
+        <hr/>
+        <div className="justify-content-end"><Button variant="outline-success">Adicionar ao carrinho!</Button></div>
+      </Alert>
+    );
+  } else{
+    return null;
+  }
 }
 
 export default AlertaProm;
