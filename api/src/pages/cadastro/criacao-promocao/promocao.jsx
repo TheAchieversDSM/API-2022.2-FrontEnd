770c4fbe7ce2bf59e235512fee3c1c060aabc8f8
Mariana Tamay <maritamay4@gmail.com>
2022-11-04 17:57:40 -0300

    Finalização da integração da página criar promoção Co-authored-by: Maria Clara Alves de Faria <mclaralvs@gmail.com> Co-authored-by: Matheus.Sakuragui <MatheusSakuragui@users.noreply.github.com>

--- api/src/pages/cadastro/criacao-promocao/promocao.jsx
+++ api/src/pages/cadastro/criacao-promocao/promocao.jsx
@@ -1,172 +1,195 @@
-import React, { useEffect, useState } from 'react';
-import Navigation from '../../../components/navbar';
-import { Alert } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Sidebar from '../../../components/sidebar';
-import Select from 'react-select';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Navigation from "../../../components/navbar";
+import { Alert } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Sidebar from "../../../components/sidebar";
+import Select from "react-select";
+import axios from "axios";
 
-import './promocao.css'
+import "./promocao.css";
 
-const modeloPromocao = [
-    { value: '', label: '' }
-];
+const modeloPromocao = [{ value: "", label: "", preco: "" }];
 
-const promocaoModelo={id:"", preco:"" , promocao:{'nome': ''}}
+const promocaoModelo = { id: "", preco: "", nome: "" };
 
 export default function Promocao() {
-    const [promocoes,setPromocoes] = useState(promocaoModelo)
+  const [promocoes, setPromocoes] = useState(promocaoModelo);
+  const [pacotes, setPacotes] = useState([promocaoModelo]);
 
-    let listaPromocao = []
+  let listaPromocao = [];
 
-    const [formValue, setFormValue] = useState([{
+  const [formValue, setFormValue] = useState([
+    {
+      promocaoNome: "",
+      promocaoPreco: "",
+      promocaoPacote: listaPromocao,
+    },
+  ]);
+
+  const duplicarTab = (event) => {
+    if (event.key === "Tab") {
+      let newfield = {
         promocaoNome: "",
         promocaoPreco: "",
-        promocaoPacotes: listaPromocao
-    }]);
+        promocaoPacote: "",
+      };
+      setFormValue([...formValue, newfield]);
+    }
+  };
 
-    const duplicarTab = (event) => {
-        if (event.key === 'Tab') {
-            let newfield = { promocaoNome: "", promocaoPreco: "", promocaoPacotes: "" }
-            setFormValue([...formValue, newfield])
-        }
+  const handleChangePacotes = (index, event) => {
+    let data = [...formValue];
+    var promocaoPac = [];
+    event.map((info) =>{
+      let pacote = {
+        id: info.value,
+        nome: info.label,
+        preco: info.preco,
+      };
+      console.log(pacote);
+      promocaoPac.push(pacote);
+    })
+
+    /*for (let i = 0; i < event.length; i++) {
+      let pacote = {
+        id: event[index].value,
+        preco: event[index].preco,
+        nome: event[index].label,
+      };
+      console.log(pacote);
+      promocaoPac.push(pacote);
+    }*/
+
+    data[index].promocaoPacote = promocaoPac;
+
+    setFormValue(data);
+  };
+
+  const handleChange = (index, event) => {
+    let data = [...formValue];
+    data[index][event.target.name] = event.target.value;
+    setFormValue(data);
+
+    console.log(formValue);
+  };
+
+  useEffect(() => {
+    async function render() {
+      axios
+        .get(`http://localhost:8080/pacotes/pegarTodosPacotes`)
+        .then((res) => {
+          var pac = [];
+          for (let index = 0; index < res.data.length; index++) {
+            let option = {
+              value: res.data[index].id,
+              label: res.data[index].nome,
+              preco: res.data[index].preco,
+            };
+            pac.push(option);
+          }
+          setPacotes(pac);
+        });
+    }
+    render();
+  }, []);
+
+  const { promocaoNome, promocaoPreco, promocaoPacote } = formValue;
+
+  const handleSubmit = (event) => {
+    let data = [...formValue];
+
+    for (let i = 0; i < data.length; i++) {
+      const promocao = {
+        nome: data[i].promocaoNome,
+        preco: data[i].promocaoPreco,
+        pacotes: data[i].promocaoPacote,
+      };
+
+      event.preventDefault();
+
+      axios
+        .post(`http://localhost:8080/promocoes/criarPromocao`, promocao)
+        .then((res) => {
+          alert("Promoção criada!");
+        });
     }
 
-    const handleChangePromocao= (event) => {
-        var promocoesSelecionados =  []
-        for (let index = 0; index < event.length; index++) {
-            let promocaoIdPreco = event[index].value.split(',')
-            let promocaoInfos = event[index].label.split('-')
-            let promocao = {id: promocaoIdPreco[0], preco: promocaoIdPreco[1], promocao:{nome: promocaoInfos[1]}  }
-            promocoesSelecionados.push(promocao)
-        }
-
-        setFormValue((prevState) => {
-            return {
-                ...prevState,
-                promocaoPacotes: promocoesSelecionados,
-            };
-        });
-    };
-    
-    const handleChange = (index, event) => {
-        let data = [...formValue];
-        data[index][event.target.name] = event.target.value;
-        setFormValue(data)
-
-        console.log(formValue);
+    let valores = {
+      promocaoNome: "",
+      promocaoPreco: "",
+      promocaoPacote: "",
     };
 
-    useEffect(()=>{
-        async function render() { 
-        axios.get(`http://localhost:8080/ofertas/pegarTodasOfertas`).then((res)=>{
-                var promocoes = []
-                for (let index = 0; index < res.data.length; index++) {
-                    let option = {
-                        value: res.data[index].id + ',' + res.data[index].preco ,
-                        label: `R$ ${res.data[index].preco}-${res.data[index].promocao.nome}`
-                    }
-                    promocoes.push(option)
-                }
-                setPromocoes(promocoes)
-            })
-        }
-        render()
-    },[])
+    setFormValue([valores]);
+  };
 
-    const { promocaoNome, promocaoPreco, promocaoPacotes } = formValue;
+  return (
+    <>
+      <div className="container-promo">
+        <h1>Cadastro de Promoções</h1>
 
-    const handleSubmit = (event) => {
-        const promocao = {
-            nome: promocaoNome,
-            preco: promocaoPreco,
-            ofertas: promocaoPacotes
-        }
+        <Form>
+          {formValue.map((fields, index) => {
+            return (
+              <div key={index}>
 
-        event.preventDefault();
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Nome da promoção</Form.Label>
+                    <Form.Control
+                      required
+                      name="promocaoNome"
+                      value={fields.promocaoNome}
+                      onChange={(event) => handleChange(index, event)}
+                      type="text"
+                      placeholder="Insira o nome da promoção"
+                    />
+                  </Form.Group>
+                </Row>
 
-        axios.post(`http://localhost:8080/promocoes/criarPromocao`, promocao).then((res) => {
-            alert('Promoção Criada!');
-        })
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Valor do desconto</Form.Label>
+                    <Form.Control
+                      required
+                      name="promocaoPreco"
+                      value={fields.promocaoPreco}
+                      onChange={(event) => handleChange(index, event)}
+                      type="number"
+                      placeholder="Insira o valor de desconto da promoção"
+                    />
+                  </Form.Group>
+                </Row>
 
-        let valores = {
-            promocaoNome: "",
-            promocaoPreco: "",
-            promocaoPacotes: listaPromocao
-        }
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="6">
+                    <Form.Label>Pacotes que compõem a promoção</Form.Label>
+                    <Select
+                      isMulti
+                      name="promocaoPacote"
+                      options={pacotes}
+                      onKeyDown={(event) => duplicarTab(event)}
+                      onChange={(event) => handleChangePacotes(index, event)}
+                      isClearable={true}
+                      isSearchable={true}
+                      closeMenuOnSelect={false}
+                    />
+                  </Form.Group>
+                </Row>
 
-        setFormValue(valores);
-    };
-    
+              </div>
+            );
+          })}
 
-    return (
-        <>
-            <div className='container-promo'>
+          <Button type="submit" onClick={handleSubmit}>
+            Criar Promoção!
+          </Button>
 
-                <h1>Cadastro de Promoções</h1>
-
-                <Form /*noValidate validated={validated} onSubmit={handleSubmit}*/>
-                {formValue.map((fields, index) => {
-                        return (
-                    <div key={index}>
-                        <Row className="mb-3">
-                        
-                            <Form.Group as={Col} md="6">
-                                <Form.Label>Nome da Promoção</Form.Label>
-                                <Form.Control
-                                    required
-                                    name="promocaoNome"
-                                    value={fields.promocaoNome}
-                                    onChange={event => handleChange(index, event)}
-                                    type="text"
-                                    placeholder="Insira o nome da Promoção"
-                                />
-                            </Form.Group>
-                        </Row>
-                        <Row className="mb-3">
-                            <Form.Group as={Col} md="6">
-                                <Form.Label>Valor do Desconto</Form.Label>
-                                <Form.Control
-                                    required
-                                    name="promocaoPreco"
-                                    value={fields.promocaoPreco}
-                                    onChange={event => handleChange(index, event)}
-                                    type="number"
-                                    placeholder="Insira o valor de desconto da Promoção"
-                                    defaultValue=""
-                                />
-                            </Form.Group>
-                        </Row>
-                        <Row className="mb-3">
-                            <Form.Group as={Col} md="6">
-                                <Form.Label>Pacotes que compõem a Promoção</Form.Label>
-                                <Select
-                                    isMulti
-                                    name="promocaoPacotes"
-                                    options={promocoes}
-                                    onKeyDown={event => duplicarTab(event)}
-                                    onChange={event => handleChangePromocao(index, event)}
-                                    isClearable={true}
-                                    isSearchable={true}
-                                    closeMenuOnSelect ={false}
-                                    value={fields.promocaoPacotes}
-                                />
-                            </Form.Group>
-                        
-                        </Row>
-                        
-                    </div>
-                    )
-                })}
-                    <Button type="submit" onClick={handleSubmit}>Criar Promoção!</Button>
-                
-                </Form>
-
-            </div>
-            
-        </>
-    )
+        </Form>
+      </div>
+    </>
+  );
 }
